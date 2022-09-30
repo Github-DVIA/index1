6ec7cdadf998a4fa0dea89bd442eecfd02358056
边缘坐标 <68896486+Edge-coordinates@users.noreply.github.com>
2022-09-30 11:44:10 +0800

    20220930

--- src/assets/js/me.js
+++ src/assets/js/me.js
@@ -7,13 +7,15 @@ setInterval(function () {
 }, 500);
 
 for (i = 0; i <= document.getElementsByTagName("a").length; i++) {
-  document
-    .getElementsByTagName("a")
-    [i].setAttribute(
-      "href",
-      document.getElementsByTagName("a")[i].href +
-        ("?" + Math.ceil(Math.random() * 10000))
-    ); //输出该页面的所有链接。
+  if (new RegExp("^(http|https)").test(document.getElementsByTagName("a")[i].href)) {
+    document
+      .getElementsByTagName("a")
+      [i].setAttribute(
+        "href",
+        document.getElementsByTagName("a")[i].href +
+          ("?" + Math.ceil(Math.random() * 10000))
+      ); //输出该页面的所有链接。
+  }
 }
 // if (new RegExp("^(http|https)").test(cover))
 // ("?" + Math.ceil(Math.random() * 10000))
